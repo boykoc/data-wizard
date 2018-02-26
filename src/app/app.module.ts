f418f28a29ef93b1d1e68a55115e9ebafe04536a
Cody Boyko <codywboyko@gmail.com>
2018-02-26 10:19:35 -0500

    Add step component and display.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StepComponent } from './step/step.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepComponent
   ],
   imports: [
     BrowserModule
